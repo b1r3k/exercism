892d450570ac192ce7c14bfa03affcf45ade8065
Lukasz Jachym <lukasz.jachym@gmail.com>
2019-10-24 13:15:55 +0200

    fixes: simplify student loop & other feedback

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -3,24 +3,23 @@ STUDENTS = ('Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet
             'Larry')
 
 
-class Garden(object):
+class Garden:
     plants_types = ['grass', 'clover', 'radishes', 'violets']
     cup_to_plant = {plant_type[0].upper(): plant_type.capitalize() for plant_type in plants_types}
 
     def __init__(self, diagram, students=None):
-        self._students_plants = self.student_to_plants(diagram.split('\n'), sorted(students or STUDENTS))
+        self._students_plants = self.student_to_plants(diagram.splitlines(), sorted(students or STUDENTS))
 
     def student_to_plants(self, diagram, students):
         students2plant = {}
         all_students_plants = {}
-        for student_idx, col_start in enumerate(range(0, len(diagram[0]), 2)):
-            students_plants = students2plant.setdefault(students[student_idx], [])
-            students_plants.append(diagram[0][col_start])
-            students_plants.append(diagram[0][col_start + 1])
-            students_plants.append(diagram[1][col_start])
-            students_plants.append(diagram[1][col_start + 1])
+        col = 0
         for student in students:
-            all_students_plants[student] = [self.cup_to_plant[cup] for cup in students2plant.get(student, [])]
+            students_plants = students2plant.setdefault(student, [])
+            students_plants.extend(diagram[0][col:col + 2])
+            students_plants.extend(diagram[1][col:col + 2])
+            all_students_plants[student] = [self.cup_to_plant[cup] for cup in students2plant[student]]
+            col += 2
         return all_students_plants
 
     def plants(self, student):
