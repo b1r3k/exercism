f45a1ffe404163c92723e4380d817c05ff3f46a8
Lukasz Jachym <lukasz.jachym@gmail.com>
2019-10-28 13:18:07 +0100

    final refactors for kindergarten

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,24 +1,21 @@
-
-STUDENTS = ('Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid',
-            'Larry')
-
-
 class Garden:
-    plants_types = ['grass', 'clover', 'radishes', 'violets']
-    cup_to_plant = {plant_type[0].upper(): plant_type.capitalize() for plant_type in plants_types}
+    STUDENTS = ('Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid',
+                'Larry')
+    CUP_TO_PLANT = {'G': 'Grass', 'C': 'Clover', 'R': 'Radishes', 'V': 'Violets'}
 
     def __init__(self, diagram, students=None):
-        self._students_plants = self.student_to_plants(diagram.splitlines(), sorted(students or STUDENTS))
+        self._students = sorted(students or self.STUDENTS)
+        self._students_plants = self.student_to_plants(diagram.splitlines(), self._students)
 
     def student_to_plants(self, diagram, students):
-        students2plant = {}
         all_students_plants = {}
         col = 0
         for student in students:
-            students_plants = students2plant.setdefault(student, [])
-            students_plants.extend(diagram[0][col:col + 2])
-            students_plants.extend(diagram[1][col:col + 2])
-            all_students_plants[student] = [self.cup_to_plant[cup] for cup in students2plant[student]]
+            students_plants = all_students_plants.setdefault(student, [])
+            first_row_cups = diagram[0][col:col + 2]
+            second_row_cups = diagram[1][col:col + 2]
+            students_plants.extend(self.CUP_TO_PLANT[cup] for cup in first_row_cups)
+            students_plants.extend(self.CUP_TO_PLANT[cup] for cup in second_row_cups)
             col += 2
         return all_students_plants
 
